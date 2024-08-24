14e29a60b7dec51b1e295d2046536547a579b0fe
Mk <matveyk201015@gmail.com>
2024-08-24 12:59:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,17 @@ let outputBox = document.querySelector("#cssOutput");
 outputBox.style.display = "none";
 
 const cssOrder = [
-    'display', 'flex-direction', 'align-items', 'justify-content', 'position', 'top', 'right', 'bottom', 'left',
-    'float', 'clear', 'visibility', 'opacity', 'z-index',
-    'margin', 'margin-top', 'margin-right', 'margin-bottom', 'margin-left',
-    'outline', 'border', 'border-radius', 'border-top', 'border-right', 'border-bottom', 'border-left',
-    'border-width', 'border-top-width', 'border-right-width', 'border-bottom-width', 'border-left-width',
-    'border-style', 'border-top-style', 'border-right-style', 'border-bottom-style', 'border-left-style',
-    'border-color', 'border-top-color', 'border-right-color', 'border-bottom-color', 'border-left-color',
-    'background', 'background-color', 'background-image', 'background-repeat', 'background-position',
-    'cursor', 'padding', 'padding-top', 'padding-right', 'padding-bottom', 'padding-left',
-    'width', 'min-width', 'max-width', 'height', 'min-height', 'max-height',
-    'overflow', 'list-style', 'caption-side', 'table-layout', 'border-collapse', 'border-spacing', 'empty-cells',
-    'vertical-align', 'text-align', 'text-indent', 'text-transform', 'text-decoration', 'line-height',
-    'word-spacing', 'letter-spacing', 'white-space', 'color', 'font', 'font-family', 'font-size', 'font-weight', 'user-select',
-    'content', 'quotes', 'transition'
+    'display', 'flex-direction', 'align-items', 'justify-content', 'position', 'top', 'right', 'bottom', 'left', 'float', 
+    'clear', 'visibility', 'opacity', 'z-index', 'margin', 'margin-top', 'margin-right', 'margin-bottom', 'margin-left', 
+    'outline', 'border', 'border-radius', 'border-top', 'border-right', 'border-bottom', 'border-left', 'border-width', 
+    'border-top-width', 'border-right-width', 'border-bottom-width', 'border-left-width', 'border-style', 'border-top-style', 
+    'border-right-style', 'border-bottom-style', 'border-left-style', 'border-color', 'border-top-color', 'border-right-color', 
+    'border-bottom-color', 'border-left-color', 'background', 'background-color', 'background-image', 'background-repeat', 
+    'background-position', 'padding', 'padding-top', 'padding-right', 'padding-bottom', 'padding-left', 'width', 'min-width', 
+    'max-width', 'height', 'min-height', 'max-height', 'cursor', 'overflow', 'list-style', 'caption-side', 'table-layout', 
+    'border-collapse', 'border-spacing', 'empty-cells', 'vertical-align', 'text-align', 'text-indent', 'text-transform', 
+    'text-decoration', 'line-height', 'word-spacing', 'letter-spacing', 'white-space', 'color', 'font', 'font-family', 
+    'font-size', 'font-weight', 'user-select', 'content', 'quotes', 'transition'
 ];
 
 function organize() {
